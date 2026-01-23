91ce69b2641e9223e9633a2e16a73554eccaad1b
Siddharth <siddharthmohanty23@gmail.com>
2026-01-23 10:40:58 +0530

    added vercel frontend

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://insightboard-pr9e.onrender.com"
+        "https://insightboard-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
